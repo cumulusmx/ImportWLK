b12abb0fcbf9cc538a01ef04f8f3a1586c0008d2
Mark Crossley <1196094+mcrossley@users.noreply.github.com>
2026-05-12 08:58:57 +0100

    Fix sep in log file unixtimestamp

--- LogFile.cs
+++ LogFile.cs
@@ -340,11 +340,11 @@ namespace ImportWLK
 			// make sure solar max is calculated for those stations without a solar sensor
 			Program.LogDebugMessage("DoLogFile: Writing log entry for " + rec.LogTime);
 			var inv = CultureInfo.InvariantCulture;
-			var sep = ",";
+			var sep = ',';
 
 			var sb = new StringBuilder(256);
 			sb.Append(rec.LogTime.ToString("dd/MM/yy HH:mm", inv));
-			sb.Append(sep + new DateTimeOffset(rec.LogTime).ToUnixTimeSeconds());
+			sb.Append(sep + new DateTimeOffset(rec.LogTime).ToUnixTimeSeconds().ToString());
 			sb.Append(sep + rec.Temperature.ToString(Program.Cumulus.TempFormat, inv));
 			sb.Append(sep + rec.Humidity.ToString());
 			sb.Append(sep + rec.Dewpoint.ToString(Program.Cumulus.TempFormat, inv));
